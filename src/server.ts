adab405f479069f30ed8caa7db88a84820c01ede
khh-Niloy <khhniloy0@gmail.com>
2025-07-25 21:51:33 +0600

    project setup with express and mongoose

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import { app } from "./app"
+import mongoose from "mongoose";
+
+const startServer = async()=>{
+    await mongoose.connect(process.env.MONGO_URI as string);
+    console.log("✅ mongoose connected");
+    app.listen(8000, ()=>{
+        console.log("server is running at 8000")
+    })
+}
+
+startServer()
